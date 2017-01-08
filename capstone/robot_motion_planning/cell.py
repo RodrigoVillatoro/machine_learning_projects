46dc85c92a54afec05746ea71a61004a9e586feb
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2017-01-08 07:49:37 +0100

    refactor: clean cell.py file - Consolidated top, middle, bottom functions by adding a double=False parameter - Updated docstrings and comments

--- capstone/robot_motion_planning/cell.py
+++ capstone/robot_motion_planning/cell.py
@@ -5,23 +5,23 @@ class Cell:
 
     def __init__(self, real_walls=None, distance=0, visited=''):
         """
-        Simplified representation of the maze for debugging purposes
-        :param walls: array of 0's (no walls) and 1's (walls) [l, u, r, d]
-        :param distance: distance to the center of the maze
-        :param visited: directions = < ^ > V; x = dead end, * = visited
+        Represents a particular cell in the maze
+        :param real_walls: array of 0's (no walls) and 1's (walls) [l, u, r, d]
+        :param distance: distance from that cell to the center of the maze
+        :param visited: str; directions = < ^ > V; x = dead end, * = visited
         """
         if real_walls is None:
             real_walls = [0, 0, 0, 0]
 
         self.real_walls = real_walls  # Actual walls on the maze
-        self.imaginary_walls = [0, 0, 0, 0]  # Walls added by the program
-        self.distance = distance
-        self.visited = visited
+        self.imaginary_walls = [0, 0, 0, 0]  # Walls added to avoid dead ends
+        self.distance = distance  # Distance to the center of the maze
+        self.visited = visited  # Used to know if robot has visited the cell
 
     def get_total_walls(self):
         """
-        Include imaginary walls in walls
-        :return: total walls... real and imaginary
+        Get real and imaginary walls for a particular cell
+        :return: array of 0s and 1s with real and imaginary walls
         """
         total_walls = [0, 0, 0, 0]
         for i in range(len(self.real_walls)):
@@ -29,10 +29,12 @@ class Cell:
                 total_walls[i] = 1
         return total_walls
 
-    def top(self):
+    def top(self, double=False):
         """
-        Returns the formatted info of the top of the cell (wall or not wall).
-        Used to print the maze.
+        Gets the info for the top of the cell (wall or not wall).
+        Used to print the maze or represent individual cells.
+        :param double: only True if we want to print individual cell. Not used
+         to print the whole maze since adjacent cells share walls
         :return: str with the first line that will visually represent a cell
         """
         cell = '+'
@@ -42,13 +44,17 @@ class Cell:
             cell += ' ... '
         else:
             cell += '     '
+        if double:
+            cell += '+'
 
         return cell
 
-    def middle(self):
+    def middle(self, double=False):
         """
-        Returns the formatted info of the center of the cell (wall, distance,
-        visited). Used to print the maze.
+        Gets info for the center of the cell (wall, distance, visited).
+        Used to print the maze or represent individual cells.
+        :param double: only True if we want to print individual cell. Not used
+         to print the whole maze since adjacent cells share walls
         :return: str with the second line that will visually represent a cell
         """
         if self.distance == WALL_VALUE:
@@ -70,13 +76,22 @@ class Cell:
             cell += ' '
         cell += self.visited
         cell += ' '
+        if double:
+            if self.real_walls[2]:
+                cell += '|'
+            elif self.imaginary_walls[2]:
+                cell += ':'
+            else:
+                cell += ' '
 
         return cell
 
-    def bottom(self):
+    def bottom(self, double=False):
         """
-        Returns the formatted info of the bottom of the cell (wall or not
-        wall). Used to print the maze.
+        Get info for the bottom of the cell (wall or not wall).
+        Used to print the maze or represent individual cells.
+        :param double: only True if we want to print individual cell. Not used
+         to print the whole maze since adjacent cells share walls
         :return: str with the third line that will visually represent a cell
         """
         cell = '+'
@@ -86,83 +101,21 @@ class Cell:
             cell += ' ... '
         else:
             cell += '     '
-
-        return cell
-
-    def top_double(self):
-        """
-        Returns the formatted info of the top of the cell (wall or not wall).
-        Used to print the maze.
-        :return: str with the first line that will visually represent a cell
-        """
-        cell = '+'
-        if self.real_walls[1]:
-            cell += ' --- '
-        elif self.imaginary_walls[1]:
-            cell += ' ... '
-        else:
-            cell += '     '
-        cell += '+'
-
-        return cell
-
-    def middle_double(self):
-        """
-        Returns the formatted info of the center of the cell (wall, distance,
-        visited). Used to print the maze.
-        :return: str with the second line that will visually represent a cell
-        """
-        distance = str(self.distance)
-        cell = ''
-        if self.real_walls[0]:
-            cell += '|'
-        elif self.imaginary_walls[0]:
-            cell += ':'
-        else:
-            cell += ' '
-        cell += ' '
-        if len(distance) == 1:
-            cell += ' '
-        cell += distance
-        if len(self.visited) == 0:
-            cell += ' '
-        cell += self.visited
-        cell += ' '
-        if self.real_walls[2]:
-            cell += '|'
-        elif self.imaginary_walls[2]:
-            cell += ':'
-        else:
-            cell += ' '
-
-        return cell
-
-    def bottom_double(self):
-        """
-        Returns the formatted info of the bottom of the cell (wall or not
-        wall). Used to print the maze.
-        :return: str with the third line that will visually represent a cell
-        """
-        cell = '+'
-        if self.real_walls[3]:
-            cell += ' --- '
-        elif self.imaginary_walls[3]:
-            cell += ' ... '
-        else:
-            cell += '     '
-        cell += '+'
+        if double:
+            cell += '+'
 
         return cell
 
     def __str__(self):
         """
-        Returns an individual cell, not used in the print_maze function.
+        Used to represent an individual cell with all of it's walls.
+        Not used to print the maze, mostly for debug.
         """
         cell = '\n'
-        cell += self.top_double()
+        cell += self.top(double=True)
         cell += '\n'
-        cell += self.middle_double()
+        cell += self.middle(double=True)
         cell += '\n'
-        cell += self.bottom_double()
+        cell += self.bottom(double=True)
         cell += '\n'
         return cell
